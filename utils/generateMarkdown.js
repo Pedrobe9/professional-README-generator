4b80d689b3f632ef519e5c00d438e4f4715f1c31
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-02-28 17:55:58 +0000

    Adding generateMarkdown function -initial simple version

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,16 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## ${data.description}
+  ## ${data.contentTable}
+  ## ${data.installation}
+  ## ${data.usage}
+  ## ${data.credits}
+  ## ${data.license}
+  ## ${data.badges}
+  ## ${data.features}
+  ## ${data.contribution}
+  ## ${data.test}
 `;
 }
 
