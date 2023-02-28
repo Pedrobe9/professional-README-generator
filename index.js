f4f1d6d9fe3090ecb20c81909bc17c6bd2cd9b9e
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-02-28 17:07:59 +0000

    Adding array of objects questions containing prompts for user to introduce information

--- index.js
+++ index.js
@@ -1,40 +1,71 @@
 const fs = require("fs");
+// Path module provides a way of working with directories and file paths
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+// Util module provides access to some utility functions
+const util = require('util');
+// promisify takes a function following the common error-first callback style, i.e. taking a (err, value) => ... 
+// callback as the last argument, and returns a version that returns promises.
+const writeToFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
     {
-        type: 'input',
-        name: 'name',
-        message: '',
-      },
-      {
-        type: 'input',
-        name: 'location',
-        message: '',
-      },
-      {
-        type: 'input',
-        name: 'hobby',
-        message: '',
-      },
-      {
-        type: 'input',
-        name: 'food',
-        message: '',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: '',
-      },
-      {
-        type: 'input',
-        name: 'linkedin',
-        message: '',
-      },
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a description for your project:',
+    },
+    {
+      type: 'input',
+      name: 'contentTable',
+      message: 'Add a table of content to make easy for users to find what they are looking for:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Describe the steps to install the porject:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructuios on how to use the project, inclusing examples and screenshots:'
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Provide information of your collaborators with links to their GitHub accounts:',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'What type of license do you want to include in your project?'
+    },
+    {
+      type: 'input',
+      name: 'badges',
+      message: 'Include any badges to show credentials:'
+    }, 
+    {
+      type: 'input',
+      name: 'features',
+      message: 'List the features of your project:'
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Write how anyone can contribute to your project:'
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Write tests for your application and provide examples on how to run them:'
+    },
 ];
 
 // function to write README file
